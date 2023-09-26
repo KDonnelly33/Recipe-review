b86405d437d0594de0b7ad5a9743f8b5fd39bbf1
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-09-26 18:54:33 -0500

    fixed error

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
+const withAuth = require('../../utils/auth');
 
 
-router.use('/comments', commentRoutes);
+router.use('/comments', withAuth, commentRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const mypageRoutes = require('./mypageRoutes');
+const withAuth = require('../utils/auth');
 
 router.use('/', homeRoutes);
-router.use('/mypage', mypageRoutes);
+router.use('/mypage', withAuth,  mypageRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
