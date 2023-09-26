c8661d20812204a36cb5be16cb14ea97a8bb1ed6
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-09-25 21:14:41 -0500

    added ability to make new post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
+const recipeRoutes = require('./recipeRoutes');
 
-
+router.use('/recipe', recipeRoutes);
 router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 
