4cd2d175b1a7da474f1a6a314e78a99592a804ec
Yiseo Kwon <yiseo1122@gmail.com>
2023-09-27 18:50:33 -0500

    Added hamburger menu for mobile and an edit button to mypage

---
+++ public/js/common.js
@@ -0,0 +1,7 @@
+var burgerMenu = document.querySelector(".hamburger-menu");
+var overlay = document.querySelector(".navigation-bar");
+
+burgerMenu.addEventListener("click", function () {
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
